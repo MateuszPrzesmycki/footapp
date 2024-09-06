fde6b17fc5e5665dc5b6767bcd83acd9c180d185
mati9412 <mati9412@op.pl>
2024-09-06 13:05:49 +0200

    clean architecture projects added

---
+++ FootApi.Domain/Entities/Club.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootApi.Domain.Entities
+{
+    internal class Club
+    {
+        public int Id { get; set; }
+        public string ClubName { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string StadiumName { get; set; } = default!;
+        public int FoundedYear { get; set; }
+        public List<Player> Players { get; set; } = new List<Player>();
+    }
+}
---
+++ FootApi.Domain/Entities/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace FootApi.Domain.Entities
+{
+    internal class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public DateTime DateOfBirth { get; set; }
+        public string Nationality { get; set; } = default!;
+        public string Position { get; set; } = default!;
+        public float Height { get; set; }
+        public float Weight { get; set; }
+
+        public Club? CurrentClub { get; set; } 
+    }
+}
